d2d8d0e02ec41807238aaf4a1185fde40d2b7802
akulau <akulau@student.uef.fi>
2022-09-06 16:17:55 +0300

    Lisätty Bookdao:n muutama query

--- app/src/main/java/com/example/kirjasovellus/database/BookDao.java
+++ app/src/main/java/com/example/kirjasovellus/database/BookDao.java
@@ -3,11 +3,21 @@ package com.example.kirjasovellus.database;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 @Dao
 public interface BookDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(Book... books);
+
+    @Query("SELECT * FROM book")
+    Book[] getAllBooks();
+
+    @Query("SELECT * FROM book WHERE BookId = :id")
+    Book[] getBookOnId(int id);
+
+    @Query("SELECT * FROM book WHERE title LIKE '%' || :title || '%'")
+    Book[] getBookOnTitle(String title);
 
 }
