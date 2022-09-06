9543ee1692df7c4e3a575918050b41cfed787049
akulau <akulau@student.uef.fi>
2022-09-06 16:18:29 +0300

    Lisätty Tietokantaan TypeConverterit

--- app/src/main/java/com/example/kirjasovellus/database/BookDatabase.java
+++ app/src/main/java/com/example/kirjasovellus/database/BookDatabase.java
@@ -2,8 +2,10 @@ package com.example.kirjasovellus.database;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {Book.class, Genre.class}, version = 1)
+@TypeConverters({Book.Converters.class})
 public abstract class BookDatabase extends RoomDatabase {
     public abstract BookDao bookDao();
     public abstract GenreDao genreDao();
