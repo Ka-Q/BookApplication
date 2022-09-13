ea015fecbe9e8ec1cac35b404103b4375ec238aa
akulau <akulau@student.uef.fi>
2022-09-13 12:57:33 +0300

    Lisätty simppeli näkymä yksittäisen kirjan tarkasteluun

--- app/src/main/java/com/example/kirjasovellus/BookDetailsFragment.java
+++ app/src/main/java/com/example/kirjasovellus/BookDetailsFragment.java
@@ -2,57 +2,24 @@ package com.example.kirjasovellus;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BookDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.kirjasovellus.database.Book;
+import com.example.kirjasovellus.database.Genre;
+
+import java.util.Date;
+
 public class BookDetailsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public BookDetailsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BookDetailsFragment newInstance(String param1, String param2) {
-        BookDetailsFragment fragment = new BookDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +27,56 @@ public class BookDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_book_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Alustava kirja, jolla on oletustiedot
+        Book book = new Book();
+        book.title = "Title";
+        book.genreIds = new int[0];
+        book.pageCount = 0;
+        book.finished = false;
+        book.finishDate = null;
+
+        // Ottaa argumenteista bundlen ja asettaa kirja-olioon
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            book  = (Book) bundle.getParcelable("book");
+        }
+
+        // Hakee kirjan genreId:illä genret ja asettaa Merkkijonoon
+        String bookGenresString = "";
+        for (int id : book.genreIds) {
+            Genre g = MainActivity.bookDatabase.genreDao().getGenresOnId(id)[0];
+            bookGenresString += g.symbol + " " + g.name + "\n";
+        }
+
+        // Tarkistaa, onko käyttäjä merkannut kirjan luetuksi. Jos on, niin näyttää päivämäärän
+        String finishedString = "Finished?: ";
+        if (book.finished) {
+            Date finishDate = book.finishDate;
+            String dateString = finishDate.toLocaleString();
+            String[] dateSplit = dateString.split(" ");
+            dateString = dateSplit[0] + " " + dateSplit[1] + " " + dateSplit[2];
+
+            finishedString += "Yes, finished on " + dateString;
+        } else {
+            finishedString += "No";
+        }
+
+        // Layout komponentit
+        TextView tvBookDetailsTitle = getView().findViewById(R.id.tvBookDetailsTitle);
+        TextView tvBookDetailsGenres = getView().findViewById(R.id.tvBookDetailsGenres);
+        TextView tvBookDetailsPageCount = getView().findViewById(R.id.tvBookDetailsPageCount);
+        TextView tvBookDetailsfinished = getView().findViewById(R.id.tvBookDetailsFinished);
+
+        tvBookDetailsTitle.setText(book.title);
+        tvBookDetailsGenres.setText(bookGenresString);
+        tvBookDetailsPageCount.setText("Page count: " + book.pageCount);
+        tvBookDetailsfinished.setText(finishedString);
+
     }
 }
