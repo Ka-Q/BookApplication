907eeada062fe40779ea80f196f5054a4c0acfb2
akulau <akulau@student.uef.fi>
2022-09-13 13:30:20 +0300

    Lisätty Napit luetuksi merkkaamiseen ja muokkaukseen

--- app/src/main/java/com/example/kirjasovellus/BookDetailsFragment.java
+++ app/src/main/java/com/example/kirjasovellus/BookDetailsFragment.java
@@ -9,11 +9,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.kirjasovellus.database.Book;
 import com.example.kirjasovellus.database.Genre;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class BookDetailsFragment extends Fragment {
@@ -48,10 +50,10 @@ public class BookDetailsFragment extends Fragment {
         }
 
         // Hakee kirjan genreId:illä genret ja asettaa Merkkijonoon
-        String bookGenresString = "";
+        String bookGenresString = "Genres:";
         for (int id : book.genreIds) {
             Genre g = MainActivity.bookDatabase.genreDao().getGenresOnId(id)[0];
-            bookGenresString += g.symbol + " " + g.name + "\n";
+            bookGenresString +=  "\n" + g.symbol + " " + g.name;
         }
 
         // Tarkistaa, onko käyttäjä merkannut kirjan luetuksi. Jos on, niin näyttää päivämäärän
@@ -72,11 +74,51 @@ public class BookDetailsFragment extends Fragment {
         TextView tvBookDetailsGenres = getView().findViewById(R.id.tvBookDetailsGenres);
         TextView tvBookDetailsPageCount = getView().findViewById(R.id.tvBookDetailsPageCount);
         TextView tvBookDetailsfinished = getView().findViewById(R.id.tvBookDetailsFinished);
+        Button btnMarkAsFinished = getView().findViewById(R.id.btnMarkAsFinished);
+        Button btnEditBook = getView().findViewById(R.id.btnEditBook);
 
+        // Asetetaan dataa Layout komponenteille kirjan perusteella
         tvBookDetailsTitle.setText(book.title);
         tvBookDetailsGenres.setText(bookGenresString);
         tvBookDetailsPageCount.setText("Page count: " + book.pageCount);
         tvBookDetailsfinished.setText(finishedString);
+        if (!book.finished) {
+            btnMarkAsFinished.setText("Mark as finished");
+        } else {
+            btnMarkAsFinished.setText("Mark as unfinished");
+        }
+
+        // Jos kirja merkattu luetuksi, merkaa lukemattomaksi ja toisinpäin. Asettaa nykyisen ajan
+        // päivämääräksi. Tallentaa tiedot tietokantaan ja "päivittää" sivun.
+        Book finalBook = book;
+        btnMarkAsFinished.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!finalBook.finished) {
+                    finalBook.finished = true;
+                    finalBook.finishDate = Calendar.getInstance().getTime();
+                } else {
+                    finalBook.finished = false;
+                    finalBook.finishDate = null;
+                }
+                MainActivity.bookDatabase.bookDao().insertAll(finalBook);
+                MainActivity.fragmentManager.beginTransaction()
+                        .replace(R.id.contentContainer, BookDetailsFragment.class, bundle)
+                        .addToBackStack("back")
+                        .commit();
+                MainActivity.fragmentManager.popBackStack();
+            }
+        });
+
+        btnEditBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.fragmentManager.beginTransaction()
+                        .replace(R.id.contentContainer, EditBookFragment.class, bundle)
+                        .addToBackStack("back")
+                        .commit();
+            }
+        });
 
     }
 }
