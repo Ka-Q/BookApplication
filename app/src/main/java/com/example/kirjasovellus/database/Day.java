d0d000c35c75a4fab291089990a5ed706677d4ea
akulau <akulau@student.uef.fi>
2022-09-15 14:20:44 +0300

    Lisätty tietokantaa päivä, jolla on päivämäärä ja luetut tunnnit

---
+++ app/src/main/java/com/example/kirjasovellus/database/Day.java
@@ -0,0 +1,31 @@
+package com.example.kirjasovellus.database;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
+
+import java.util.Date;
+
+@Entity
+public class Day implements Parcelable {
+
+    @PrimaryKey()
+    public Date date;
+
+    @ColumnInfo
+    public double hours;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+
+    }
+}
---
+++ app/src/main/java/com/example/kirjasovellus/database/DayDao.java
@@ -0,0 +1,16 @@
+package com.example.kirjasovellus.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface DayDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(Day... days);
+
+    @Query("SELECT * FROM day ORDER BY date ASC")
+    Day[] getAllDays();
+}
